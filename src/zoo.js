fad61f2a5c92c3a026519865935f59f69ed07cfd
Helena <nenafgreco@hotmail.com>
2021-08-04 17:45:32 -0300

    Requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -1,11 +1,14 @@
 const { species } = require('./data');
 const data = require('./data');
+const { employees } = require('./data');
 
 function getSpeciesByIds(...ids) { // todos os elementos id como parâmetro.
   // seu código aqui
   const speciesByIds = species.filter(({ id }) => ids.includes(id)); // cria um novo array que incluem no nome (id)
   return speciesByIds;
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function getAnimalsOlderThan(animal, ages) {
   // seu código aqui
@@ -16,6 +19,13 @@ function getAnimalsOlderThan(animal, ages) {
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  // reduzir o array pelo nome passado, se for igual ao primeiro nome OU ultimo nome, chamar o obj, se não, retornar o obj vazio.
+  const employeeByName = employees.reduce((accumulator, currentValue) => {
+    if (currentValue.firstName === employeeName || currentValue.lastName === employeeName) {
+      return currentValue;
+    } return accumulator;
+  }, {}); // {} -> valor inicial do array que estou criando.
+  return employeeByName;
 }
 
 function createEmployee(personalInfo, associatedWith) {
